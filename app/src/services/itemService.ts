700d7d9f078053abe78e554c90a3cc1e011b3539
Vijay Sharma <Sharmaa76Vijay@gmail.com>
2019-10-23 20:30:48 +0530

    Searching on name basis implemented

--- app/src/services/itemService.ts
+++ app/src/services/itemService.ts
@@ -1,9 +1,22 @@
-import { getManager } from 'typeorm'
+import { getManager, Like } from 'typeorm'
 import { Item } from '../entity/item'
+import { Category } from 'src/entity/category'
 
 export class ItemService {
-  async getAllItems (pageno: number) {
-    const items = await getManager().getRepository(Item).find({ skip: (pageno - 1) * 5, take: 5 })
+  async getAllItems (pageno: number, name: string, category: string) {
+    let items
+    if (name != null || name != undefined || category != null || category != undefined) {
+      if (name != null || name != undefined) {
+        items = await getManager().getRepository(Item).find({ where: { 'name': Like('%' + name + ' #%') }, skip: (pageno - 1) * 5, take: 5 })
+      }
+      const subquery = await getManager()
+        .createQueryBuilder(Item, 't1')
+        .innerJoin(Category, 't2', '"t1".g = "t2".category_id ')
+        .where('"t4".k = 4 AND ("t6".i = 2 OR ("t6".i = 1 AND "t6".j = 1))')
+        .getMany()
+    } else {
+      items = await getManager().getRepository(Item).find({ skip: (pageno - 1) * 5, take: 5 })
+    }
     for (var k in items) {
       items[k].categories = await getManager()
         .createQueryBuilder()
